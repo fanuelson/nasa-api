970b9fd9e9683aad9f867b08509da74e30dddcb7
ff <fanuelson@gmail.com>
2020-02-03 17:53:34 -0300

    created temperature conversor utils

---
+++ src/main/java/com/preoday/utils/TemperatureUtils.java
@@ -0,0 +1,8 @@
+package com.preoday.utils;
+
+public final class TemperatureUtils {
+
+	public static Double celsiusToFahrenheit(Double celsiusTemp) {
+		return (celsiusTemp * (9.0 / 5.0)) + 32.0;
+	}
+}
