da97986564fe9c67453af9d3ade96c5552b6ed0d
ff <fanuelson@gmail.com>
2020-02-03 17:57:31 -0300

    refactored hard code strings to constants and start using fahrenheit

--- src/main/java/com/preoday/service/NasaService.java
+++ src/main/java/com/preoday/service/NasaService.java
@@ -6,11 +6,15 @@ import org.springframework.stereotype.Service;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.node.ArrayNode;
 import com.preoday.apirequest.NasaApiRequester;
+import com.preoday.utils.TemperatureUtils;
 import com.preoday.vo.TemperatureResponseVO;
 
 @Service()
 public class NasaService {
 
+	private static final String SOL_KEYS_FIELD = "sol_keys";
+	private static final String AV_FIELD = "av";
+	private static final String AT_FIELD = "AT";
 	@Autowired
 	private NasaApiRequester nasaApiRequester;
 	
@@ -27,15 +31,16 @@ public class NasaService {
 	public TemperatureResponseVO getMarsTemperatureAverage(Integer sol) {
 		JsonNode nasaApiResponse = nasaApiRequester.requestMarsWeather();
 
-		ArrayNode solKeys = nasaApiResponse.withArray("sol_keys");
+		ArrayNode solKeys = nasaApiResponse.withArray(SOL_KEYS_FIELD);
 
 		Double averageTemperature = 0.0;
 		if (sol != null && checkIfExists(solKeys, String.valueOf(sol))) {
-			averageTemperature = nasaApiResponse.get(String.valueOf(sol)).get("AT").get("av").asDouble();
+			Double solAverage = nasaApiResponse.get(String.valueOf(sol)).get(AT_FIELD).get(AV_FIELD).asDouble();
+			averageTemperature = TemperatureUtils.celsiusToFahrenheit(solAverage);
 		} else {
 			for (JsonNode solKey : solKeys) {
-				Double average = nasaApiResponse.get(solKey.asText()).get("AT").get("av").asDouble();
-				averageTemperature += average;
+				Double solAverage = nasaApiResponse.get(solKey.asText()).get(AT_FIELD).get(AV_FIELD).asDouble();
+				averageTemperature += TemperatureUtils.celsiusToFahrenheit(solAverage);
 			}
 			averageTemperature = averageTemperature / solKeys.size();
 		}
